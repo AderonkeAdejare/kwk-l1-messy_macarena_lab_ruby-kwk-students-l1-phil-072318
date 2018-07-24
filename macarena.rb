ddc8d5feda2909c64103ca2cb89269b22d1eaa19
AderonkeAdejare <aderonkea@comcast.net>
2018-07-24 14:17:59 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -17,16 +17,14 @@ puts "Step 3: Turn your right palm up."
 # Step 4: GOOD
 puts "Step 4: Turn your left palm up."
 
-# Step 5: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+# Step 5: GOOD
+puts "Step 5: Place your right hand on your left shoulder."
 
-puts "Step 5: Bring your right hand up and around to the back of your head."
-
-# Step 6: FIXME
-
+# Step 6: GOOD
+puts "Step 6: Place your left hand on your right shoulder."
 
 # Step 7: FIXME
-puts "Step 6: Place your left hand on your right shoulder."
+puts "Step 5: Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
 puts "Step 8: The Last Step"
